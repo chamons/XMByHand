7343ba3be9e03c3185e3877b4feff8bc5841529b
Chris Hamons <chris.hamons@xamarin.com>
2018-07-13 14:58:42 -0500

    Add sample

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using AppKit;
+
+namespace XMByHand
+{
+	class MainClass
+	{
+		[DllImport ("/usr/lib/libSystem.dylib")]
+		static extern IntPtr dlopen (string path, int mode);
+
+		[DllImport ("/usr/lib/libSystem.dylib")]
+		static extern IntPtr dlerror ();
+
+		public static void Main (string[] args)
+		{
+			var dylibPath = Path.Combine ("/Library/Frameworks/Xamarin.Mac.framework/Versions/Current/lib/libxammac.dylib");
+			if (dlopen (dylibPath, 0) == IntPtr.Zero) {
+				var errPtr = dlerror ();
+				var errStr = (errPtr == IntPtr.Zero) ? "<unknown error>" : Marshal.PtrToStringAnsi (errPtr);
+				Console.WriteLine ("WARNING: Cannot load {0}: {1}", dylibPath, errStr);
+			}
+
+			NSApplication.Init ();
+			var v = NSApplication.SharedApplication;
+			Console.WriteLine (v == null);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+This is a small sample showing how to startup Xamarin.Mac "by hand".
+
+It is not a supported approach, and if things break you get to keep the pieces.
